911c3deb3c68a8fbad7f5e331b11f99a36fe7c86
spurlats <55952181+spurlats@users.noreply.github.com>
2021-02-08 19:07:30 +0400

    add class BasePage

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,17 @@
 from selenium.webdriver.common.by import By
 import time
 
-# def test_find_basket(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#     browser.get(link)
-#     time.sleep(10)
-#     item = browser.find_element(By.CSS_SELECTOR, (".btn.btn-lg.btn-primary.btn-add-to-basket"))
-#     assert item is not None, "No such element Exception"
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR,'#login_link')
+    login_link.click()
+
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    go_to_login_page(browser)
 
 # pytest -v --tb=line --language=en test_main_page.py
